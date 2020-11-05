4357a5fd6e1f59b66a2b466350d867add5695551
김건형 <show11837@gmail.com>
2020-11-05 14:00:00 +0900

    feat : device 설정 (iOS Deployment Target)
    iOS Deployment Target : 13.6

--- Notepad.ver3/SceneDelegate.swift
+++ Notepad.ver3/SceneDelegate.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
+@available(iOS 13.0, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -48,9 +48,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
 }
-
