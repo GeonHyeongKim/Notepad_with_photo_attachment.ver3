8591c407b8e9115eaf49f385ddeb5dceeec5348d
김건형 <show11837@gmail.com>
2020-11-06 13:23:45 +0900

    feat : MemoListVC > table view data source(_:cellForRowAt:)
    1. memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼냄
    2. 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
    3. 재사용 큐로부터 프로토타입 셀의 인스턴스를 전달받음
    4. memoCell의 내용을 구성
    5. Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포멧으로 변경
    6. cell 객체를 반환

--- Notepad.ver3/MemoListVC.swift
+++ Notepad.ver3/MemoListVC.swift
@@ -9,82 +9,40 @@
 import UIKit
 
 class MemoListVC: UITableViewController {
-
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let count = self.appDelegate.memoList.count
+        return count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        // memolist 배열 데이터에서 주어진 행에 맞는 데이터를 꺼냄
+        let row = self.appDelegate.memoList[indexPath.row]
+        
+        // 이미지 속성이 비어 있을 경우 "memoCell", 아니면 "memoCellWithImage"
+        let cellId = row.image == nil ? "memoCell" : "memoCellWithImage"
+        
+        // 재사용 큐로부터 프로토타입 셀의 인스턴스를 전달받음
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId) as! MemoCell
+        
+        // memoCell의 내용을 구성
+        cell.lblSubject?.text = row.title
+        cell.lblContents?.text = row.contents
+        cell.ivThum?.image = row.image
+        
+        // Date 타입의 날짜를 yyyy-MM-dd HH:mm:ss 포멧으로 변경
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.lblRegdate?.text = formatter.string(from: row.regdate!)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
