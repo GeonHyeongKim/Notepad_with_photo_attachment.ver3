55d7d2a631d26fe83ddb279ab13ff68ee1f35296
김건형 <show11837@gmail.com>
2020-11-06 09:56:38 +0900

    feat : Custom Class 생성 및 객체 연결
    UITableViewController > MemoListVC : 목록
    UIViewController > MemoFromVC : 메모
    UIViewController > MemoReadVC : 상세화면
    TableViewCell > MemoCell : 목록 List

---
+++ Notepad.ver3/UITableViewCell/MemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  MemoCell.swift
+//  Notepad.ver3
+//
+//  Created by gunhyeong on 2020/11/06.
+//  Copyright © 2020 geonhyeong. All rights reserved.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
