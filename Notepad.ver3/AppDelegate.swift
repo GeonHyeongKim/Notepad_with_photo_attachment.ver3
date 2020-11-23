0aa9a4f76d4da48a87bf2d72405196f77fa6abf9
김건형 <show11837@gmail.com>
2020-11-23 11:07:04 +0900

    syle : app launching 시간 1초 -> 0초

--- Notepad.ver3/AppDelegate.swift
+++ Notepad.ver3/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        Thread.sleep(forTimeInterval: 1.0)
+//        Thread.sleep(forTimeInterval: 1.0)
         print("Database FilePath: ", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last ?? "Not Found")
         return true
     }
