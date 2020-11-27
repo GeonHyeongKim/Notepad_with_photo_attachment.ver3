59a94fedc09b02ea20cc9ee6107074af487d27b6
김건형 <show11837@gmail.com>
2020-11-27 15:37:58 +0900

    style : NewAppDelegate 메소드 및 코드 작성 - 탭 바 컨트롤러 생성 및 배경 변경

--- Notepad.ver3/NewAppDelegate.swift
+++ Notepad.ver3/NewAppDelegate.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2020 geonhyeong. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+@UIApplicationMain
+class NewAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        // 1. TabBar Controller를 생성 하고, 배경을 흰색
+        let tbC = UITableViewController()
+        tbC.view.backgroundColor = UIColor.white
+        
+        return true
+    }
+}
