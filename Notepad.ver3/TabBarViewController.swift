c3e56373ca87b857fe9a5624ab8a336288a47c8f
김건형 <show11837@gmail.com>
2020-11-28 20:06:05 +0900

    feat : touchesEnded 화면에서 터치가 끝났을 때 호출

--- Notepad.ver3/TabBarViewController.swift
+++ Notepad.ver3/TabBarViewController.swift
@@ -16,15 +16,15 @@ class TabBarViewController: UITabBarController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 화면에서 터치가 끝났을 때 호출
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let tabBar = self.tabBarController?.tabBar
+//        tabBar?.isHidden = (tabBar?.isHidden == true) ? false : true
+        
+        UIView.animate(withDuration: TimeInterval(0.15)) {
+            // alpha 값이 0이면 1로, 1이면 0으로 변경
+            // 호출될 때마다 점점 투명해졌다가 점점 진해짐
+            tabBar?.alpha = (tabBar?.alpha == 0 ? 1 : 0)
+        }
     }
-    */
-
 }
